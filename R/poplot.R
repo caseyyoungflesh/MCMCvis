d91c42bd0d9b47a0954b85ea003163287704d8db
Caseyyoungflesh <Casey.youngflesh@stonybrook.edu>
2016-03-23 18:45:36 -0400

    Fix function problem with dependencies
    Update documentation with examples and citations

--- R/poplot.R
+++ R/poplot.R
@@ -1,10 +1,28 @@
 #' Plots posterior distributions
 #'
+#' Function plots the posterior distributions obtained from MCMC
+#' chains output from a Bayesian model.
+#'
 #' @param data Matrix of MCMC chains
 #' @param LML Lower bounds x-axis
 #' @param LMH upper bounds x-axis
 #' @section Details:
 #' Chains should be in columns of input matrix
+#' @section Notes:
+#' Wrapper for denstrip package, as highlighted by Jackson (2008). Example can be seen in
+#' Zipkin et al. 2014, figure 3.
+#' @section References:
+#' Jackson, C. H. 2008. Displaying Uncertainty With Shading. The American Statistician 62:340-347.
+#'
+#' Zipkin, E. F., T. S. Sillett, E. H. C. Grant, R. B. Chandler,
+#' and J. A. Royle. 2014. Inferences about population dynamics
+#' from count data using multistate models: a comparison to
+#' capture-recapture approaches. Ecology and Evolution 4:417-426.
+#' @examples
+#' x1 <- rnorm(1000, mean=0.5)
+#' x2 <- rnorm(1000, mean=0)
+#' data <- cbind(x1, x2)
+#' poplot(data)
 #'
 #' @export
 
@@ -12,10 +30,15 @@
 poplot <- function(data, LML = -1, LMH= 1) #WID= height of density strips, LML, LMH = x-axis bounds
 {
   #for debug
-  WID=.2
+  #-------------#
+  #WID=.2
   #LML = -1
   #LMH = 1
-  #data <- OR_chains_beta
+  #x1 <- rnorm(1000, mean=.5)
+  #x2 <- rnorm(1000, mean=0)
+  #data <- cbind(x1,x2)
+  #-------------#
+
 
   chains <- as.data.frame(data)
 
@@ -25,20 +48,22 @@ poplot <- function(data, LML = -1, LMH= 1) #WID= height of density strips, LML, 
   # apply and sort labels
   labs = colnames(data)[idx]
 
-  mp= reshape2::melt(chains[,idx])
+  mp= suppressMessages(reshape2::melt(chains[,idx], value.name='value'))
 
   #x11()
-  rpp = lattice::bwplot(variable~value,data=mp,
+  rpp = bwplot(variable~value,data=mp,
                xlab=list(label="Parameter probability values",cex=1.3),
                xlim=c(LML, LMH),
                panel = function(x, y)
                {
                  #grid.segments(1,0,0,0)
+                 xlist <- split(mp$value, factor(mp$variable))
                  xlist <- split(x, factor(y))
+
                  for (i in seq(along=xlist))
                  {
                    #panel.grid(h=c(0), col='grey')
-                   denstrip::panel.denstrip(x=xlist[[i]], at=i, width= WID)#, colmax='white', colmin= 'white')
+                   denstrip::panel.denstrip(x=xlist[[i]], at=i, width= WID, colmax='black', colmin= 'white')
 
                  }
                }, par.settings = list(axis.line = list(col=NA)),
