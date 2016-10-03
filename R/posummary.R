5a8965ff0a82f474d091c98d920cad8c59bba9fd
Casey Youngflesh <casey.youngflesh@stonybrook.edu>
2016-10-03 15:38:31 -0400

    Add excl, fix printing order bug, clean up

--- R/posummary.R
+++ R/posummary.R
@@ -39,279 +39,193 @@
 #' @export
 #' @import coda
 object <- fit
+object <- SD_out
 posummary <- function(object,
-                      par = c('eta','mu'),
-                      excl = 'theta',
+                      par = c('all'),
+                      excl = NULL,
                       Rhat = TRUE)
 {
-  if(typeof(object) == 'S4')
+  if(typeof(object) == 'list' & coda::is.mcmc.list(object) == FALSE)
   {
-    object2 <- rstan::As.mcmc.list(object)
+    #modified from R2jags package
+    x <- object$BUGSoutput
+    mclist <- vector("list", x$n.chains)
+    mclis <- vector("list", x$n.chains)
+    strt <- x$n.burnin + 1
+    end <- x$n.iter
+    ord <- dimnames(x$sims.array)[[3]]
+    for (i in 1:x$n.chains)
+    {
+      tmp1 <- x$sims.array[, i, ord]
+      mclis[[i]] <- mcmc(tmp1, start = strt, end = end, thin = x$n.thin)
+    }
+    object2 <- as.mcmc.list(mclis)
+
   } else {
-    object2 <- object
+    if(typeof(object) == 'S4')
+    {
+      object2 <- rstan::As.mcmc.list(object)
+    } else {
+      object2 <- object
+    }
   }
 
-     if(coda::is.mcmc.list(object2) == TRUE)
+  if(coda::is.mcmc.list(object2) == TRUE)
+  {
+    temp <- object2
+    names <- colnames(temp[[1]])
+
+    ch_bind <- do.call('rbind', temp)
+
+    bind_mn <- apply(ch_bind, 2, mean)
+    bind_LCI <- apply(ch_bind, 2, quantile, probs= 0.025)
+    bind_med <- apply(ch_bind,2, median)
+    bind_UCI <- apply(ch_bind, 2, quantile, probs= 0.975)
+    r_hat <- coda::gelman.diag(temp)$psrf[,1]
+
+    mcmc_summary <- cbind(bind_mn, bind_LCI, bind_med, bind_UCI, r_hat)
+    colnames(mcmc_summary) <- c('mean','2.5%','50%','97.5%', 'Rhat')
+ }
+
+  if(typeof(object2) == 'double')
+  {
+    temp <- object2
+    names <- colnames(temp)
+
+    bind_mn <- apply(temp, 2, mean)
+    bind_LCI <- apply(temp, 2, quantile, probs= 0.025)
+    bind_med <- apply(temp,2, median)
+    bind_UCI <- apply(temp, 2, quantile, probs= 0.975)
+    if(Rhat == TRUE)
     {
-      temp <- object2
-      names <- colnames(temp[[1]])
+      warning('Rhat statistic cannot be calculated without individaul chains. NAs inserted.')
+    }
+    r_hat <- rep(NA, NCOL(temp))
 
-      ch_bind <- do.call('rbind', temp)
+    mcmc_summary <- cbind(bind_mn, bind_LCI, bind_med, bind_UCI, r_hat)
+    colnames(mcmc_summary) <- c('mean','2.5%','50%','97.5%', 'Rhat')
+  }
 
-      bind_mn <- apply(ch_bind, 2, mean)
-      bind_LCI <- apply(ch_bind, 2, quantile, probs= 0.025)
-      bind_med <- apply(ch_bind,2, median)
-      bind_UCI <- apply(ch_bind, 2, quantile, probs= 0.975)
-      r_hat <- coda::gelman.diag(temp)$psrf[,1]
+  if(!is.null(excl))
+  {
+    to.rm1 <- c()
+    for (i in 1:length(excl))
+    {
+      to.rm1 <- c(to.rm1, grep(excl[i], names, fixed = TRUE))
+    }
+    dups <- -which(duplicated(to.rm1))
+    if(length(dups) > 0)
+    {
+      to.rm2 <- to.rm1[-dups]
+    }else{
+      to.rm2 <- to.rm1
+    }
+  }
 
-      mcmc_summary <- cbind(bind_mn, bind_LCI, bind_med, bind_UCI, r_hat)
-      colnames(mcmc_summary) <- c('mean','2.5%','50%','97.5%', 'Rhat')
+  if (length(par) == 1)
+  {
+    if (par == 'all')
+    {
+      if(is.null(excl))
+      {
+        OUT <- mcmc_summary
+      }else{
+        OUT <- mcmc_summary[-to.rm2,]
+      }
+    }else
+     {
+      get.rows <- grep(paste(par), names, fixed=TRUE)
+      if (length(get.rows) < 1)
+      {
+        stop(paste0('"', par, '"', ' not found in MCMC ouput.'))
+      }
 
       if(!is.null(excl))
       {
-        to.rm1 <- c()
-        for (i in 1:length(excl))
+        if(identical(get.rows, to.rm2))
         {
-          to.rm1 <- c(to.rm1, grep(excl[i], names, fixed = TRUE))
-        }
-        dups <- -which(duplicated(to.rm1))
-        if(length(dups) > 0)
-        {
-          to.rm2 <- to.rm1[-dups]
-        }else{
-          to.rm2 <- to.rm1
-        }
-      }
-
-
-      if (length(par) == 1)
-      {
-        if (par == 'all')
-        {
-          if(is.null(excl))
-          {
-            OUT <- mcmc_summary
-          }else{
-            OUT <- mcmc_summary[-to.rm2,]
-          }
-        }else
-        {
-          get.rows <- grep(paste(par), names, fixed=TRUE)
-          if (length(get.rows) < 1)
-          {
-            stop(paste0('"', par, '"', ' not found in MCMC ouput.'))
-          }
-
-          if(!is.null(excl))
-          {
-            if(identical(get.rows, to.rm2))
-            {
-              stop('No parameters selected.')
-            }
-
-            matched <- which(get.rows == to.rm2)
-            if (length(matched) > 0)
-            {
-              g_filt <- get.rows[-matched]
-            }else {
-              g_filt <- get.rows
-            }
-
-          }else{
-            g_filt <- get.rows
-          }
-
-          OUT <- mcmc_summary[g_filt,]
-        }
-      }else {
-        grouped <- c()
-        for (i in 1:length(par))
-        {
-          get.rows <- grep(paste(par[i]), names, fixed=TRUE)
-          if (length(get.rows) < 1)
-          {
-            stop(paste0('"', par[i], '"', ' not found in MCMC ouput.'))
-          }
-          grouped <- c(grouped, get.rows)
+          stop('No parameters selected.')
         }
 
-        if(!is.null(excl))
+        matched <- which(get.rows == to.rm2)
+        if (length(matched) > 0)
         {
-          if(identical(grouped, to.rm2))
-          {
-            stop('No parameters selected.')
-          }
-
-          matched <- suppressWarnings(which(grouped == to.rm2))
-          if (length(matched) > 0)
-          {
-            rows <- grouped[-matched]
-          } else{
-            rows <- grouped
-          }
-
-          to.rm <- which(duplicated(rows))
-          if(length(to.rm) > 0)
-          {
-            g_filt <- rows[-to.rm]
-          }else
-          {
-            g_filt <- rows
-          }
-
-          OUT <- mcmc_summary[g_filt,]
-
-        } else{
-
-          to.rm <- which(duplicated(grouped))
-          if(length(to.rm) > 0)
-          {
-            g_filt <- grouped[-to.rm]
-          }else
-          {
-            g_filt <- grouped
-          }
-
-          OUT <- mcmc_summary[g_filt,]
+          g_filt <- get.rows[-matched]
+        }else {
+          g_filt <- get.rows
         }
-      }
 
+      }else{
+        g_filt <- get.rows
+       }
 
-
-    if(typeof(object2) == 'double')
+      OUT <- mcmc_summary[g_filt,]
+    }
+  }else {
+    grouped <- c()
+    for (i in 1:length(par))
     {
-      temp <- object2
-      names <- colnames(temp)
-
-      bind_mn <- apply(temp, 2, mean)
-      bind_LCI <- apply(temp, 2, quantile, probs= 0.025)
-      bind_med <- apply(temp,2, median)
-      bind_UCI <- apply(temp, 2, quantile, probs= 0.975)
-      if(Rhat == TRUE)
+      get.rows <- grep(paste(par[i]), names, fixed=TRUE)
+      if (length(get.rows) < 1)
       {
-        warning('Rhat statistic cannot be calculated without individaul chains. NAs inserted.')
+        stop(paste0('"', par[i], '"', ' not found in MCMC ouput.'))
       }
-      r_hat <- rep(NA, NCOL(temp))
-
-      mcmc_summary <- cbind(bind_mn, bind_LCI, bind_med, bind_UCI, r_hat)
-      colnames(mcmc_summary) <- c('mean','2.5%','50%','97.5%', 'Rhat')
-
-
-      if (length(par) == 1)
-      {
-        if (par == 'all')
-        {
-          OUT <- mcmc_summary
-        }else
-        {
-          get.rows <- grep(paste(par), names, fixed=TRUE)
-          if (length(get.rows) < 1)
-          {
-            stop(paste0('"', par, '"', ' not found in MCMC ouput.'))
-          }
-          OUT <- mcmc_summary[get.rows,]
-        }
-      }else
-      {
-        grouped <- c()
-        for (i in 1:length(par))
-        {
-          get.rows <- grep(paste(par[i]), names, fixed=TRUE)
-          if (length(get.rows) < 1)
-          {
-            stop(paste0('"', par[i], '"', ' not found in MCMC ouput.'))
-          }
-          grouped <- c(grouped, get.rows)
-        }
-
-        to.rm <- which(duplicated(grouped))
-        if(length(to.rm) >0)
-        {
-          g_filt <- grouped[-to.rm]
-        }else
-        {
-          g_filt <- grouped
-        }
-
-        OUT <- mcmc_summary[g_filt,]
-      }
-
+      grouped <- c(grouped, get.rows)
     }
 
-    if(typeof(object2) == 'list' & coda::is.mcmc.list(object2) == FALSE)
+    if(!is.null(excl))
     {
-      temp <- object2$BUGSoutput$summary
-      names <- rownames(temp)
-
-      if (length(par) == 1)
+      if(identical(grouped, to.rm2))
       {
-        if (par == 'all')
-        {
-          OUT <- temp[, c(1,3,5,7,8)]
-        }else
-        {
-          get.rows <- grep(paste(par), names, fixed=TRUE)
-          if (length(get.rows) < 1)
-          {
-            stop(paste0('"', par, '"', ' not found in MCMC ouput.'))
-          }
-          OUT <- temp[get.rows, c(1,3,5,7,8)]
-        }
+        stop('No parameters selected.')
+      }
+
+      matched <- na.omit(match(to.rm2, grouped))
+      if (length(matched) > 0)
+      {
+        rows <- grouped[-matched]
+      } else{
+        rows <- grouped
+      }
+
+      to.rm <- which(duplicated(rows))
+      if(length(to.rm) > 0)
+      {
+        g_filt <- rows[-to.rm]
       }else
       {
-        grouped <- c()
-        for (i in 1:length(par))
-        {
-          get.rows <- grep(paste(par[i]), names, fixed=TRUE)
-          if (length(get.rows) < 1)
-          {
-            stop(paste0('"', par[i], '"', ' not found in MCMC ouput.'))
-          }
-          grouped <- c(grouped, get.rows)
-        }
-
-        to.rm <- which(duplicated(grouped))
-        if(length(to.rm) >0)
-        {
-          g_filt <- grouped[-to.rm]
-        }else
-        {
-          g_filt <- grouped
-        }
-
-        OUT <- temp[g_filt, c(1,3,5,7,8)]
+        g_filt <- rows
       }
+      OUT <- mcmc_summary[g_filt,]
+    } else{
+
+      to.rm <- which(duplicated(grouped))
+      if(length(to.rm) > 0)
+      {
+        g_filt <- grouped[-to.rm]
+      }else
+      {
+        g_filt <- grouped
+      }
+
+      OUT <- mcmc_summary[g_filt,]
     }
+  }
 
-
-  if(coda::is.mcmc.list(object) != TRUE &
-     typeof(object) != 'double' &
-     typeof(object) != 'list' &
-     typeof(object) != 'S4')
+  if(coda::is.mcmc.list(object2) != TRUE &
+     typeof(object2) != 'double' &
+     typeof(object2) != 'list' &
+     typeof(object2) != 'S4')
   {
       stop('Invalid object type. Input must be stanfit object, mcmc.list object, rjags object, or matrix with MCMC chains.')
   }
 
 
-  if(is.null(dim(OUT)))
+  if(Rhat == TRUE)
   {
-    if(Rhat == TRUE)
-    {
-      return(OUT)
-    }else
-    {
-      return(OUT[-5])
-    }
+    return(OUT)
   }else
   {
-    if(Rhat == TRUE)
-    {
-      return(OUT)
-    }else
-    {
-      return(OUT[,-5])
-    }
+    return(OUT[,-5])
   }
 }
-
-
