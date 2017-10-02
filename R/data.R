c56979da140673c8b7545b2daa84301c36efcfcd
Casey Youngflesh <caseyyoungflesh@gmail.com>
2017-10-01 21:32:54 -0400

    Update documentation - 6k iterations

--- R/data.R
+++ R/data.R
@@ -3,6 +3,6 @@
 #' Sample MCMC output containing 30 parameters - \code{alpha[1]}, ... , \code{alpha[10]},
 #' \code{beta[1]}, ... , \code{beta[10]}, \code{gamma[1]}, ... , \code{gamma[10]}.
 #'
-#' @format \code{mcmc.list} object with 3 chains for each parameter, 1000 iterations for each chain
+#' @format \code{mcmc.list} object with 3 chains for each parameter, 6000 iterations for each chain
 #'
 "MCMC_data"
